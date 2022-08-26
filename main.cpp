0bc69fa3763b33d084cc29a4cb5fce3023499adc
RADHIKA MITTAL <109732567+radhika4444@users.noreply.github.com>
2022-08-26 15:05:54 +0530

    q2

--- main.cpp
+++ main.cpp
@@ -1,51 +1,38 @@
 //name=RADHIKA MITTAL
 //rollno=2010990558
 //setno=4
-//qno=1
+//qno=2
 
 #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-void minindex(int arr[],int n){
-   map<int,int> arr1;
-   for(int i=0;i<n;i++){
-    arr1[arr[i]]++; //here keys are arr elements and value are their count when they appear in array
-   }
-
-   int ans;
-   for(auto it=arr1.begin();it!=arr1.end();it++){
-    if(it->second>=2){ //checking if count>=2 then we are storing key in ans
-        ans=it->first;
+void consecutive_array_or_not(int arr[],int n){
+    set<int> s1; //declaring array
+    for(int i=0;i<n;i++){
+        s1.insert(arr[i]); //inserting elements in set
     }
-   }
-   bool b=false;
-   int i;
-   for(i=0;i<n;i++){
-    if(arr[i]==ans){ //checking that ans is in array or not and printing that position
-        b=true;
-        break;
+    set<int>::iterator it;
+    int a=*(s1.begin()); //putting 1st element of set in a
+    int b=*(s1.rbegin());  //putting last element of set in b
+    if(b-a==n-1 && s1.size()==n){ //if size of b-a==no of elements in array means consecutive else not
+        cout<<"array contains consecutive integers of "<<a<<" to "<<b<<endl;
     }
-   }
-   if(b==true){
-    cout<<"The minimum index of the repeating element is "<<i<<endl;
-   }
-   else{
-    cout<<"Invalid Input"<<endl;
-   }
+    else{
+        cout<<"array does not contain consecutive integers as element "<<b<<" is repeated"<<endl;
+    }
 }
-
 int main()
 {
-    cout<<"enter size of array  : ";
-    int n;
-    cin>>n; //taking size as input
+    int n,t;
+    cout<<"enter size of array : ";
+    cin>>n;
     int arr[n];
     cout<<"enter elements in array : ";
     for(int i=0;i<n;i++){
         cin>>arr[i]; //taking array elements as input
     }
-    minindex(arr,n);
+    consecutive_array_or_not(arr,n);
     return 0;
 }
